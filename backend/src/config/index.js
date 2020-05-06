dff6e486cb5f481e21600e00ca8cc4c4404df9ea
Luis Antonio Rodriguez Garcia <luis_rdz5@hotmail.com>
2020-05-05 19:03:34 -0500

    [Backend] created the basic structure of an endpoint

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER || "",
-  dbPassword: process.env.DB_PASSWORD || "",
-  dbHost: process.env.DB_HOST || "",
-  dbName: process.env.DB_NAME || "",
+  mysql: {
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
+    host: process.env.DB_HOST || "",
+    name: process.env.DB_NAME || "",
+  }
 };
 
 module.exports = { config };
