c1b10ebfeeb7a092630eb0efebd57442c7f8b123
wilantury <ventas@antury.net>
2020-05-14 20:58:47 -0500

    [add] update products

--- backend/src/routes/products/network.js
+++ backend/src/routes/products/network.js
@@ -5,15 +5,27 @@ const Controller = require('./index')
 
 //Routs
 router.post('/', insert);
+router.put('/', update);
 router.get('/', list);
 router.get('/search/name', searchByName);
 router.get('/search/price', searchByPrice);
+router.get('/search/category/:id', searchByCategory);
 router.get('/:id', get);
 
 function insert(req, res, next){
     Controller.insert(req.body)
         .then((user) => {
             response.success(req, res, user, 200);
+        })
+        .catch( (err) => {
+            response.error(req, res, err.message, 500, 'error network Products');
+        });
+}
+
+function update(req, res, next){
+    Controller.update(req.body)
+        .then((res_update) => {
+            response.success(req, res, res_update, 200);
         })
         .catch( (err) => {
             response.error(req, res, err.message, 500, 'error network Products');
@@ -57,8 +69,16 @@ function searchByPrice(req, res, next){
         })
         .catch( (err) => {
             response.error(req, res, err.message, 500, 'error network SearchByName');
-        });
-    
+        });    
+}
+function searchByCategory(req, res, next){
+    Controller.getProductsByCategory(req.params.id)
+        .then((resultSearch) => {
+            response.success(req, res, resultSearch, 200);
+        })
+        .catch( (err) => {
+            response.error(req, res, err.message, 500, 'error network SearchByCategory');
+        });    
 }
 
 module.exports = router;
